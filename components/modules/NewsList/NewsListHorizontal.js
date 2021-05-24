0cfd139073c1b1cc72ec1c108a635fd9fbf026f8
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-05-24 14:20:19 +0700

    add link when click news components

--- components/modules/NewsList/NewsListHorizontal.js
+++ components/modules/NewsList/NewsListHorizontal.js
@@ -1,35 +1,38 @@
+import Link from "next/link";
 import PropTypes from "prop-types";
 
 const NewsListVertical = ({ imgSrc }) => (
-  <div className="cursor-pointer">
-    <div className="grid grid-cols-3 gap-2">
-      <div className={`${imgSrc ? "col-span1" : "hidden"}`}>
-        <img src={imgSrc} alt="news-images" className="rounded mb-2" />
-      </div>
-      <div className={`${imgSrc ? "col-span-2" : "col-span-3"}`}>
-        <p className="font-medium text-xs leading-4 text-turqoise mb-2">
-          CATEGORY
-        </p>
-        <p className="font-medium text-base text-black mb-2">
-          Singtel enters MOU with Hyundai to collaborate on smart manufacturing,
-          EV battery
-        </p>
-        <div className="flex mb-4">
-          <div className="flex-1">
-            <p className="font-normal text-xs leading-3 text-black-600">
-              1 Month Ago
-            </p>
-          </div>
-          <div className="flex-1">
-            <p className="font-normal text-xs leading-3 text-black-600 text-right">
-              12 Min Read
-            </p>
+  <Link href="/news/:slug">
+    <div className="cursor-pointer">
+      <div className="grid grid-cols-3 gap-2">
+        <div className={`${imgSrc ? "col-span1" : "hidden"}`}>
+          <img src={imgSrc} alt="news-images" className="rounded mb-2" />
+        </div>
+        <div className={`${imgSrc ? "col-span-2" : "col-span-3"}`}>
+          <p className="font-medium text-xs leading-4 text-turqoise mb-2">
+            CATEGORY
+          </p>
+          <p className="font-medium text-base text-black mb-2">
+            Singtel enters MOU with Hyundai to collaborate on smart
+            manufacturing, EV battery
+          </p>
+          <div className="flex mb-4">
+            <div className="flex-1">
+              <p className="font-normal text-xs leading-3 text-black-600">
+                1 Month Ago
+              </p>
+            </div>
+            <div className="flex-1">
+              <p className="font-normal text-xs leading-3 text-black-600 text-right">
+                12 Min Read
+              </p>
+            </div>
           </div>
         </div>
       </div>
+      <div className="mb-4 dashed-line-grey"></div>
     </div>
-    <div className="mb-4 dashed-line-grey"></div>
-  </div>
+  </Link>
 );
 
 NewsListVertical.propTypes = {
---
+++ pages/news/[slug].js
@@ -0,0 +1,14 @@
+import Head from "../../components/elements/Head/Head.js";
+import Navbar from "../../components/elements/Navbar/Navbar.js";
+
+const News = () => (
+  <>
+    <Head />
+    <Navbar />
+    <div>
+      <p className="font-bold text-base text-blue-100 mb-4">Detail News</p>
+    </div>
+  </>
+);
+
+export default News;
